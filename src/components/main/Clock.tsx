c45520f7bb348b63bda8b8a0ff037b12524b9606
hyperflow <hyperflow@kakao.com>
2021-03-05 03:08:00 +0900

    fix(main/Clock): 시간 표시 형식 수정

--- src/components/main/Clock.tsx
+++ src/components/main/Clock.tsx
@@ -1,7 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const ClockWrapper = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 1.3;
+  margin-bottom: 14px;
+`;
 
 const Clock: React.FC = () => {
   const [date, setDate] = useState(new Date());
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const period = hours > 12 ? '오후' : '오전';
+  const displayHours = hours % 12;
 
   useEffect(() => {
     setInterval(() => {
@@ -9,7 +22,11 @@ const Clock: React.FC = () => {
     }, 1000);
   }, []);
 
-  return <div>{date.toLocaleTimeString()}</div>;
+  return (
+    <ClockWrapper>
+      {period} {displayHours}시 {minutes}분 {seconds}초
+    </ClockWrapper>
+  );
 };
 
 export default Clock;
