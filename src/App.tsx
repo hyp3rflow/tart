be52174d314ac1fa2b04e4be8cb2460fdc38dde2
hyperflow <hyperflow@kakao.com>
2021-03-08 23:51:41 +0900

    chore: react-router 대신 react-router-dom 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
 import GlobalStyle from 'components/base/GlobalStyle';
 import WindowFrame from 'components/base/WindowFrame';
 import WindowTemplate from 'components/base/WindowTemplate';
